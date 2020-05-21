a23ffbcb9682a0e33a59f71434914f050d219d9e
nb-67 <65740772+nb-67@users.noreply.github.com>
2020-05-22 01:35:45 +0300

    Add files via upload

---
+++ script.py
@@ -0,0 +1,43 @@
+print("All Star Signs & their Symbols:")
+
+print("‎‏‏‎ ‎‎‎")
+
+aries = 'Aries - ♈︎'
+print(aries)
+
+tauras = 'Tauras - ♉︎'
+print(tauras)
+
+gemini = 'Gemini - ♊︎'
+print(gemini)
+
+cancer = 'Cancer - ♋︎'
+print(cancer)
+
+leo = 'Leo - ♌︎'
+print(leo)
+
+virgo = 'Virgo - ♍︎'
+print(virgo)
+
+libra = 'Libra - ♎︎'
+print(libra)
+
+scorpio = 'Scorpio - ♏︎'
+print(scorpio)
+
+sagittarius = 'Sagittarius - ♐︎'
+print(sagittarius)
+
+capricorn = 'Capricorn - ♑︎'
+print(capricorn)
+
+aquarias = 'Aquarias - ♒︎'
+print(aquarias)
+
+pisces = 'Pisces - ♓︎'
+print(pisces)
+
+print("‎‏‏‎ ‎‎‎")
+
+print("Check out my GitHub for more content: https://github.com/nb-67/")
